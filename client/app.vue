51b91a8b03fe8428d39379f6d124f53b185ea9fb
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-04-20 20:04:47 +0200

    Added tailwind and flowbite

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <NuxtWelcome />
-    </div>
+    <main class="min-h-screen bg-gray-50 text-gray-950">
+        <NuxtPage />
+    </main>
 </template>
+
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+onMounted(() => initFlowbite())
+</script>
