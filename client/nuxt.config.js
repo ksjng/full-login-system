200da1f07bd693c8cee7ece0f4a75babce62ab77
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-04-20 20:35:22 +0200

    Set api proxy

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
     modules: [
         "@nuxtjs/tailwindcss"
-    ]
+    ],
+
+    nitro: {
+        routeRules: {
+            "/api/**": {
+                proxy: "http://127.0.0.1:3001/api/**"
+            }
+        }
+    }
+
 })
