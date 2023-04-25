7ab4378f44e379821ff7477d5654d66484f04089
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-04-25 17:54:04 +0200

    Added resend activation email API endpoint

---
+++ api/routes/api/auth/resend-activation.js
@@ -0,0 +1,45 @@
+import { prisma } from "../../../index.js";
+import generateActivationEmail from "../../../utils/generateActivationEmail.js";
+
+
+export default async (fastify, options) => {
+
+    fastify.post("/resend-activation", async (req, res) => {
+
+        const { email } = req.body;
+
+        if(!email) return res.send({
+            success: false,
+            error: "Form filled incorrectly"
+        });
+
+        const user = await prisma.users.findFirst({
+            where: {
+                AND: [
+                    { 
+                        email 
+                    },
+                    { 
+                        status: 0 
+                    }
+                ]
+            }
+        });
+
+        if(!user) return res.send({
+            success: false,
+            error: "Could not found any inactive account assiciated with provided email"
+        });
+
+        const generateStatus = await generateActivationEmail(email);
+
+        if(generateStatus !== "SUCCESS") return res.send({
+            success: false,
+            error: generateStatus
+        });
+
+        res.send({ success: true });
+
+    });
+    
+}
