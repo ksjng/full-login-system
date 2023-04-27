8c754aed53e1c15888ea094187ee60e6db07a2db
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-04-27 17:15:57 +0200

    Fixed success message

--- client/components/activate/VerifyingEmail.vue
+++ client/components/activate/VerifyingEmail.vue
@@ -1,13 +1,13 @@
 <template>
 
-    <div>
+    <div class="text-center">
 
         <h1 class="font-medium text-4xl my-5">{{ status }}</h1>
 
-        <p>{{ error }}</p>
+        <p>{{ message }}</p>
 
-        <!--<button data-modal-target="resend-activation-modal" data-modal-toggle="resend-activation-modal" class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Resend activation link</button> 
--->
+        <button v-if="status == 'Email verified'" @click="navigateTo('/login')" class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log in</button> 
+
     </div>
 
 </template>
@@ -18,7 +18,9 @@
 import { ref, onMounted } from "vue";
 
 const status = ref("Verifying...");
-const error = ref("");
+const message = ref("");
+
+const route = useRoute();
 
 onMounted(async () => {
 
@@ -32,11 +34,13 @@ onMounted(async () => {
         const body = await res.json();
         if(!body.success) throw new Error(body.error);
 
-        return status.value = "Email verified";
+        status.value = "Email verified";
+        message.value = "Now you can log in to your account";
 
     } catch(err) {
         console.error(err);
-        error.value = err.message;
+        status.value = "Error";
+        message.value = err.message;
     }
 
 });
