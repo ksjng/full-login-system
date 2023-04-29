680b1d562ff557a38645484e27fff3fdf97f2b47
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-04-29 16:53:17 +0200

    Created provide email password recovery form

---
+++ client/pages/recover.vue
@@ -0,0 +1,23 @@
+<template>
+    
+    <main class="grid place-items-center min-h-screen m-auto">
+        <div class="max-w-prose w-full p-5 mt-10">
+
+            <SetNewPassword v-if="route.query.token" />
+
+            <ProvideEmail v-else />
+
+        </div>
+    </main>
+
+</template>
+
+
+<script setup>
+
+import SetNewPassword from "../components/recover/SetNewPassword.vue";
+import ProvideEmail from "../components/recover/ProvideEmail.vue";
+
+const route = useRoute();
+
+</script>
