de1b1679084fc28be31798297bee8ce32c5bddd4
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-05-01 18:15:35 +0200

    Added navbar profile item

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
 
     <nav class="fixed w-full z-20 top-0 left-0 bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-lg">
 
-            <a href="#" class="flex items-center">
+            <nuxt-link to="/" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+            </nuxt-link>
 
             <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -16,7 +16,18 @@
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+                        <a v-if="user.username" href="#" id="dropdown_profile_btn" data-dropdown-toggle="dropdown_profile" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><img :src="user.avatarUrl" class="inline w-8 rounded-full pb-[2px]" alt="Profile picture"> {{ user.username }}</a>
+                        <nuxt-link v-else to="/login" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</nuxt-link>
+                        <div id="dropdown_profile" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown_profile_btn">
+                                <li>
+                                    <nuxt-link to="/dashboard" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</nuxt-link>
+                                </li>
+                                <li>
+                                    <a @click="logout()" href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
             </div>
@@ -25,3 +36,35 @@
     </nav>
 
 </template>
+
+
+<script setup>
+
+import { ref, onMounted } from "vue";
+
+const user = ref({});
+
+onMounted(async () => {
+
+    try {
+        const res = await fetch("/api/user/me");
+
+        if(res.status == 401) return;
+
+        if(!res.ok) throw new Error("Failed to connect with API server");
+        const body = await res.json();
+        
+        user.value = body.data;
+
+    } catch(err) {
+        console.error(err);
+    }
+
+});
+
+const logout = async () => {
+    document.cookie = "authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    return await navigateTo("/logout");
+}
+
+</script>
