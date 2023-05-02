fdf9bd23ef178279c807378cae97a8fabd13253b
Kamil <80323556+Kamil6789@users.noreply.github.com>
2023-05-02 20:47:46 +0200

    Added change password api endpoint

---
+++ api/routes/api/user/settings/change-password.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+
+import { prisma } from "../../../../index.js";
+
+
+export default async (fastify, options) => {
+
+    fastify.requireAuthentication(fastify);
+
+    fastify.post("/change-password", async (req, res) => {
+
+        const { oldPassword, newPassword } = req.body;
+
+        if(!oldPassword || !newPassword) return res.send({
+            success: false,
+            error: "Form filled incorrectly"
+        });
+
+        const { username } = req.auth;
+
+        const user = await prisma.users.findFirst({
+            where: { username }
+        });
+
+        const match = await bcrypt.compare(oldPassword, user.password);
+
+        if(!match) return res.send({
+            success: false,
+            error: "Incorrect password"
+        });
+
+        const newHashedPassword = await bcrypt.hash(newPassword, 12);
+
+        await prisma.users.update({
+            where: { username },
+            data: { password: newHashedPassword }
+        });
+
+        res.send({ success: true });
+
+    });
+    
+}
